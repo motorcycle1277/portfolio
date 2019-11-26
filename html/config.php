3d920fa3e74c135cc957f534f30fd8fc76135982
mototake <igarashihajimetsuyoshi@mba.local>
2019-11-26 20:14:38 +0900

    コンフィグの訂正

--- html/config.php
+++ html/config.php
@@ -1,6 +1,5 @@
 <?php
-$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-$db['dbname'] = ltrim($db['path'], '/');
-define('DSN',"mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8");
-define('DB_USER',"$db['user']");
-define('DB_PASS',"$db['pass']");
+
+define('DSN','mysql:host=localhost;dbname=cafeben');
+define('DB_USER','user1');
+define('DB_PASS','user1Pass');
