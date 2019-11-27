a3e32b753ecb116632c3b2ed80d5e5eeb3847e50
mototake <igarashihajimetsuyoshi@mba.local>
2019-11-28 06:23:30 +0900

    commit

--- html/change.php
+++ html/change.php
@@ -12,19 +12,27 @@ if(!isset($_SESSION['EMAIL'])){
   <head>
     <meta charset="utf-8">
     <title>カフェ勉 for auwifi|会員情報変更</title>
+    <link rel="stylesheet" href="../css/style.css">
   </head>
   <body>
-    <ul>
+    <ul class="link">
       <li class="headlink"><a href="logout.php">ログアウト</a></li>
       <li class="headlink"><a href="../index.php">トップページ</a></li>
     </ul>
+    <div class="form-wrapper">
     <h1>会員情報変更</h1>
     <form action="change-output.php" method="post">
-      <label for="id">メールアドレス</label>
-      <input type="text" name="email" value="">
-      <label for="password">パスワード</label>
-      <input type="text" name="password" value="">
+      <div class="form-item">
+      <label for="id"></label>
+      <input type="email" name="email" value="" required="required" placeholder="Email">
+    </div>
+    <div class="form-item">
+      <label for="password"></label>
+      <input type="password" name="password" value="" required="required" placeholder="Password">
+    </div>
+    <div class="button-panel">
       <input type="submit" value="送信">
+    </div>
     </form>
   </body>
 </html>
